db862587359d0d043e86bc9b42892e97a270518d
Artem Zakharchenko <kettanaito@gmail.com>
2021-04-14 17:57:01 +0200

    Uses "cosmiconfig" to support various config formats

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
-import * as fs from 'fs'
-import * as path from 'path'
+import { cosmiconfigSync } from 'cosmiconfig'
 import { toJestConfig } from './converters/toJestConfig'
 import { toWebpackConfig } from './converters/toWebpackConfig'
 
-const aliasConfigPath = path.resolve(process.cwd(), '.alias.config.js')
+const explorer = cosmiconfigSync('alias')
+const result = explorer.search()
 
-if (!fs.existsSync(aliasConfigPath)) {
-  throw new Error(
-    `Failed to configure path alias: configuration not found at "${aliasConfigPath}".`
-  )
+if (!result) {
+  throw new Error(`Failed to configure path alias: configuration not found.`)
 }
 
-const aliasConfig = require(aliasConfigPath)
-
 export const alias = {
-  jest: toJestConfig(aliasConfig),
-  webpack: toWebpackConfig(aliasConfig),
+  jest: toJestConfig(result.config),
+  webpack: toWebpackConfig(result.config),
 }
